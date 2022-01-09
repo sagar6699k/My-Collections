2885f6b631f01faff0419fd010c9b6d73600d23e
Sagar Kurewar <sagarkurewar@gmail.com>
2022-01-09 18:23:45 +0530

    JS notes files

---
+++ DSA_revision/input_temp.js
@@ -0,0 +1,33 @@
+function runProgram(input) {
+
+  var array = input
+ 
+  
+
+
+
+  
+  }
+  if (process.env.USERNAME === "Sagar Kurewar") {
+    runProgram(`1
+    5
+    abcda`);
+  } else {
+    process.stdin.resume();
+    process.stdin.setEncoding("ascii");
+    let read = "";
+    process.stdin.on("data", function (input) {
+      read += input;
+    });
+    process.stdin.on("end", function () {
+      read = read.replace(/\n$/, "");
+      read = read.replace(/\n$/, "");
+      runProgram(read);
+    });
+    process.on("SIGINT", function () {
+      read = read.replace(/\n$/, "");
+      runProgram(read);
+      process.exit(0);
+    });
+}
+  
