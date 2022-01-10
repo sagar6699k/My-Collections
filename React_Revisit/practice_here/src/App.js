dec8206f1fe2df125588c15730bfc7bb128f0e54
Sagar Kurewar <sagarkurewar@gmail.com>
2022-01-10 09:57:36 +0530

    react_practice

---
+++ React_Revisit/practice_here/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import ClassCompo from './Components/ClassCompo';
+import FunctionCompo from './Components/FunctionCompo';
+
+function App() {
+  return (
+    <div className="App">
+      <FunctionCompo />
+      <ClassCompo />
+    </div>
+  );
+}
+
+export default App;
