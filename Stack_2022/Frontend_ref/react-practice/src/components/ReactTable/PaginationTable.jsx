e8acd7ae998c03833c2007409afaf33601ba24f1
SAGAR KUREWAR <sagar.kurewar@indusnet.co.in>
2022-07-19 13:52:14 +0530

    pagination gotopage added

--- Stack_2022/Frontend_ref/react-practice/src/components/ReactTable/PaginationTable.jsx
+++ Stack_2022/Frontend_ref/react-practice/src/components/ReactTable/PaginationTable.jsx
@@ -1,82 +1,104 @@
-import React, { useMemo } from 'react'
+import React, { useMemo } from "react";
 import { useTable, usePagination } from "react-table";
 import MOCK_DATA from "../MOCK_DATA.json";
 import { COLUMNS, GROUPED_COLUMN } from "./columns";
-import './basictable.css';
+import "./basictable.css";
 
 const BasicTable = () => {
+  const columns = useMemo(() => COLUMNS, []);
+  // const grouped_columns = useMemo(() => GROUPED_COLUMN, [])
+  const data = useMemo(() => MOCK_DATA, []);
 
-    const columns = useMemo(() => COLUMNS, [])
-    // const grouped_columns = useMemo(() => GROUPED_COLUMN, [])
-    const data = useMemo(() => MOCK_DATA, [])
+  const {
+    getTableProps,
+    getTableBodyProps,
+    headerGroups,
+    footerGroups,
+    page,
+    previousPage,
+    nextPage,
+    canPreviousPage,
+    canNextPage,
+    pageOptions,
+    gotoPage,
+    pageCount,
+    state,
+    prepareRow,
+  } = useTable(
+    {
+      columns,
+      data,
+    },
+    usePagination
+  );
 
-    const {
-        getTableProps,
-        getTableBodyProps,
-        headerGroups,
-        footerGroups,
-        page,
-        previousPage,
-        nextPage,
-        canPreviousPage,
-        canNextPage,
-        pageOptions,
-        state,
-        prepareRow
+  const { pageIndex } = state;
 
-    } = useTable({
-        columns,
-        data
-    }, usePagination)
+  console.log("Latest_data --->", page, pageOptions);
 
-    const {pageIndex} = state;
+  return (
+    <>
+      <table id="users_table" {...getTableProps()}>
+        <thead>
+          {headerGroups.map((headerGroup) => (
+            <tr {...headerGroup.getHeaderGroupProps()}>
+              {headerGroup.headers.map((column) => (
+                <th {...column.getHeaderProps}>{column.render("Header")}</th>
+              ))}
+            </tr>
+          ))}
+        </thead>
+        <tbody {...getTableBodyProps()}>
+          {page.map((row) => {
+            prepareRow(row);
+            return (
+              <tr {...row.getRowProps()}>
+                {row.cells.map((cell) => {
+                  return (
+                    <td {...cell.getCellProps()}>{cell.render("Cell")}</td>
+                  );
+                })}
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
 
-    console.log("Latest_data --->", page, pageOptions);
+      <div>
+        <span>
+          Page{" "}
+          <strong>
+            {pageIndex + 1} of {pageOptions.length}
+          </strong>
+        </span>
+        
+          
+        <span>
+            | Go to the page: {" "}
+                  <input type="number"
+                      defaultValue={pageIndex + 1}
+                      onChange={(e) => {
+                          const pageNumber = e.target.value ? Number(e.target.value) - 1 : 0
+                          gotoPage(pageNumber)
+                      }}
+                  />
+                      
+        </span>
+        <button onClick={() => gotoPage(0)} disabled={!canPreviousPage}>
+          {"<<"}
+        </button>
+        <button onClick={() => previousPage()} disabled={!canPreviousPage}>
+          Prev
+        </button>
+        <button onClick={() => nextPage()} disabled={!canNextPage}>
+          Next
+        </button>
+        <button onClick={() => gotoPage(pageCount-1)} disabled={!canNextPage}>
+          {">>"}
+        </button>
+      </div>
+    </>
+  );
+};
 
-    return (
-        <>
-            <table id="users_table" {...getTableProps()}>
-                <thead>
-                    {headerGroups.map((headerGroup) => (
-                        <tr {...headerGroup.getHeaderGroupProps()}>
-                            {headerGroup.headers.map((column) => (
-                                <th {...column.getHeaderProps}>{column.render('Header')}</th>
-                            ))}
-
-                        </tr>
-                    ))}
-
-                </thead>
-                <tbody {...getTableBodyProps()}>
-                    {page.map((row) => {
-                        prepareRow(row)
-                        return (
-                            <tr {...row.getRowProps()}>
-                                {
-                                    row.cells.map((cell) => {
-                                        return <td {...cell.getCellProps()}>{cell.render('Cell')}</td>
-                                    })
-                                }
-
-                            </tr>
-                        )
-                    })}
-                </tbody>
-            </table>
-
-            <div>
-
-                <span>
-                    Page{' '}
-                    <strong>
-                        {pageIndex + 1} of {pageOptions.length}
-                    </strong>
-                </span>
-                <button onClick={() => previousPage()} disabled={!canPreviousPage}>Prev</button>
-                <button onClick={() => nextPage()} disabled={!canNextPage}>Next</button>
-            </div>
-        </>
-    )
-}
-
-export default BasicTable
+export default BasicTable;
