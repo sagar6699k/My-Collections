bf1a990eb24f14d00e34f5e7c169a03def5d4169
Sagar <sagarkurewar@gmail.com>
2023-11-23 16:09:04 +0530

    adding DSA

---
+++ Stack_2023/DSA_Practice/Balance_Paranthesis/simple.js
@@ -0,0 +1,65 @@
+function balanceParanthesis(arr) {
+    const stack = [];
+    for (let i = 0; i < arr.length; i++) {
+        let top = stack.length - 1;
+        if (
+            (stack[top] === "(" && arr[i] === ")") ||
+            (stack[top] === "[" && arr[i] === "]") ||
+            (stack[top] === "{" && arr[i] === "}")
+        ) {
+            stack.pop();
+            
+        } else {
+            stack.push(arr[i])
+        }
+    }
+
+    if (stack.length === 0) {
+        console.log("Balanced");
+    } else {
+        console.log("Not Balanced");
+    }
+}
+
+function runProgram(input) {
+
+    var arr = input.trim().split("\n");
+    let T = Number(arr[0].trim());
+
+    let line = 1;
+    for (let i = 1; i <= T; i++) {
+        let array = arr[line].trim().split("");
+        line++;
+
+        balanceParanthesis(array)
+    }
+
+
+
+
+
+}
+if (process.env.USERNAME === "Sagar Kurewar") {
+    runProgram(`3
+      {([])}
+      ([]
+      [()]{}{[()()]()}
+      `);
+} else {
+    process.stdin.resume();
+    process.stdin.setEncoding("ascii");
+    let read = "";
+    process.stdin.on("data", function (input) {
+        read += input;
+    });
+    process.stdin.on("end", function () {
+        read = read.replace(/\n$/, "");
+        read = read.replace(/\n$/, "");
+        runProgram(read);
+    });
+    process.on("SIGINT", function () {
+        read = read.replace(/\n$/, "");
+        runProgram(read);
+        process.exit(0);
+    });
+}
